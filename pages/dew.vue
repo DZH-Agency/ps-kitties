43c928cc7b8bd35e97af31b49efbddc4bcc55ba9
serhii.liamtsev <serhiiliamtsev@gmail.com>
2022-07-27 12:19:06 +0300

    Add "dew" page with redirect

---
+++ pages/dew.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+  
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'dew',
+  mounted() {
+    window.location = 'https://docs.google.com/forms/d/e/1FAIpQLSd7u7_WHLtwIvZ5qnn5S9TlyM1D0OAvvUdBKzitZrvzmiwfZA/viewform?usp=sf_link'
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
