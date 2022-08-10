4179573ea224c0ce49df65de2b9c1c23e6ea3015
serhii.liamtsev <serhiiliamtsev@gmail.com>
2022-08-11 02:33:09 +0300

    Added breeding page (first tab)

---
+++ pages/breeding.vue
@@ -0,0 +1,217 @@
+<template>
+  <div>
+    <div class="header-wrapper">
+      <div class="ps-navbar-wrapper">
+        <div class="ps-container">
+          <div class="ps-navbar">
+            <nuxt-link to="/" class="ps-navbar-brand">
+              <img src="/img/breeding/logo.svg" alt="" class="ps-navbar-brand__img">
+            </nuxt-link>
+            <a href="https://breeding.psychokitties.io" target="_blank" class="ps-navbar__enter-link">
+              Enter breeding site (not live)
+            </a>
+          </div>
+        </div>
+      </div>
+      
+      <div class="ps-container">
+        <div class="header">
+          <div class="header-tabs">
+            <div class="header-tabs__tab active">
+              Redeemables
+            </div>
+            <div class="header-tabs__tab">
+              Terms of Service
+            </div>
+            <div class="header-tabs__tab">
+              FAQ
+            </div>
+          </div>
+          
+          <h1 class="header__heading">
+            This drop features two redeemable scenarios
+          </h1>
+          
+          <div class="header-about">
+            <img src="/img/breeding/number-one.svg" alt="" class="header-about__number-img">
+            
+            <div class="header-about-text">
+              <p class="header-about-text__text">
+                Every person who breeded at least 1 baby will be eligible for at least 1 of 6 first-of-its-kind comic
+                strips depending on a few factors:
+              </p>
+              <p class="header-about-text__text">
+                - people who purchased a PsychoKitty in the first 24 hours during last years PsychoKitty drop (from mint
+                or secondary) will receive the very first edition (Edition 1) of the comic series
+              </p>
+              <p class="header-about-text__text">
+                - the other 5 comics are distributed to holders depending on the amount of <span
+                class="hint what-is-popup-parent">hidden staking <img
+                src="/img/breeding/question-mark.svg" alt="">
+                <span class="what-is-popup">
+                  <strong>What is hidden staking?</strong>
+                  <br>
+                  <br>
+Each day your NFT is in your wallet, you will gain one point. If you sell your NFT, the points associated with that NFT will be erased. Total hidden staking points are calculated via sum-total of all the points of your PK, MHS and Baby NFTs.
+                </span>
+              </span> points they have accumulated by HODLing their
+                PsychoKitties and Mad Hares (5 different point brackets)
+              </p>
+            </div>
+          </div>
+          
+          <div class="header-stats">
+            <div class="header-stats-items">
+              <div class="header-stats-items-item">
+              <span class="header-stats-items-item__text">
+                <strong class="tilda">250</strong> points
+              </span>
+                <span class="header-stats-items-item__edition">
+                Edition 1
+              </span>
+              </div>
+              <div class="header-stats-items-item">
+              <span class="header-stats-items-item__text">
+                <strong>251-500</strong> points
+              </span>
+                <span class="header-stats-items-item__edition">
+                Edition 2 and 3
+              </span>
+              </div>
+              <div class="header-stats-items-item">
+              <span class="header-stats-items-item__text">
+                <strong>501-750</strong> points
+              </span>
+                <span class="header-stats-items-item__edition">
+                Edition 2,3 and 4
+              </span>
+              </div>
+              <div class="header-stats-items-item">
+              <span class="header-stats-items-item__text">
+                <strong>751-1000</strong> points
+              </span>
+                <span class="header-stats-items-item__edition">
+                Edition 2,3,4 and 5
+              </span>
+              </div>
+              <div class="header-stats-items-item">
+              <span class="header-stats-items-item__text">
+                More than <strong>1000</strong> <br> points
+              </span>
+                <span class="header-stats-items-item__edition">
+                Edition 2,3,4,5 and 6
+              </span>
+              </div>
+            </div>
+            <div class="header-stats__hint">
+              (Points table will be shared in Discord before the breeding season ends)
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    
+    <div class="staking-wrapper">
+      <div class="ps-container">
+        <div class="staking">
+          <div class="staking-about">
+            <img src="/img/breeding/number-two.svg" alt="" class="staking-about__number-img">
+            
+            <div class="staking-about-text">
+              <p class="staking-about-text__text">
+                Generation Q babies will start generating additional <span class="hint what-is-popup-parent">hidden staking <img
+                src="/img/breeding/question-mark.svg" alt="">
+                <span class="what-is-popup">
+                  <strong>What is hidden staking?</strong>
+                  <br>
+                  <br>
+Each day your NFT is in your wallet, you will gain one point. If you sell your NFT, the points associated with that NFT will be erased. Total hidden staking points are calculated via sum-total of all the points of your PK, MHS and Baby NFTs.
+                </span>
+              </span> points that will be converted into our upcoming
+                game tokens and in-game rewards next year.
+              </p>
+            </div>
+          </div>
+          
+          <div class="staking-how">
+            <div class="staking-how-guide">
+              <h2 class="staking-how-guide__title">HOW TO BREED</h2>
+              <div class="staking-how-guide-text">
+                <p class="staking-how-guide-text__text">
+                  Once you've purchased your love potions:
+                </p>
+                <p class="staking-how-guide-text__text">
+                  - head over to <a href="https://breeding.psychokitties.io" target="_blank">https://breeding.psychokitties.io</a>
+                  (if the site requires password, then our breeding
+                  section has not been opened up yet, get up-to-date information in our Discord or Twitter)
+                </p>
+                <p class="staking-how-guide-text__text">
+                  - verify your wallet according to instructions
+                </p>
+                <p class="staking-how-guide-text__text">
+                  - select your parents and a love potion
+                </p>
+                <p class="staking-how-guide-text__text">
+                  - choose your breeding style
+                </p>
+                <p class="staking-how-guide-text__text">
+                  - reveal your baby
+                </p>
+                <p class="staking-how-guide-text__text">
+                  - rinse and repeat
+                </p>
+              </div>
+              <div class="staking-how-guide-tip">
+                <strong>TIP:</strong> Screen-record your breeding process and tag <strong>@PsychoKittiesOG</strong> &
+                <strong>@MadHareSociety</strong> on Twitter to have a chance at winning a custom baby by your favorite
+                artist where you decide the traits! (3 winners after breeding season ends)
+              </div>
+            </div>
+            <img src="/img/breeding/how-to-breed.png" alt="" class="staking-how__img">
+          </div>
+          
+          <div class="staking-when">
+            <h2 class="staking-when__title">
+              WHEN WILL I RECEIVE MY BABY NFT'S AND COMICS?
+            </h2>
+            <p class="staking-when__text">
+              Generation Q babies and comics will be airdropped within a month after the breeding season is over. In
+              case we need to extend our 7-day breeding season, the redeemables will be airdropped within a month
+              starting from the new breeding end date.
+            </p>
+          </div>
+        </div>
+      </div>
+      
+      <div class="examples-wrapper">
+        <div class="examples"></div>
+      </div>
+      
+      <div class="questions-wrapper">
+        <div class="ps-container">
+          <div class="questions">
+            <p class="questions__text">
+              Have questions? Join <a href="https://www.discord.gg/psychokitties" target="_blank">www.discord.gg/psychokitties</a>
+            </p>
+            <p class="questions__text">
+              Be up to date with the latest breeding news at <a target="_blank"
+                                                                href="https://www.twitter.com/psychokittiesog">www.twitter.com/psychokittiesog</a>
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  
+  
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'breeding'
+}
+</script>
+
+<style scoped lang="scss">
+@import "@/assets/scss/breeding.scss";
+</style>
