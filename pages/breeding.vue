1635210814abfca0460f8bf269344155443c6e20
serhii.liamtsev <serhiiliamtsev@gmail.com>
2022-08-19 20:30:18 +0300

    Update navbar link in breeding

--- pages/breeding.vue
+++ pages/breeding.vue
@@ -8,7 +8,7 @@
               <img src="/img/breeding/logo.svg" alt="" class="ps-navbar-brand__img">
             </nuxt-link>
             <a href="https://breeding.psychokitties.io" target="_blank" class="ps-navbar__enter-link">
-              Enter breeding site <br class="mobile-only"> (not live)
+              LIVE in parenthesis
             </a>
           </div>
         </div>
